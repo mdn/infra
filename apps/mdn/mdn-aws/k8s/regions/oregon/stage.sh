38861b0a6ec6af9e0425e11a3eabce6f8314e735
John Whitlock <jwhitlock@mozilla.com>
2018-11-07 14:46:22 -0600

    bug 948151: Enforce CSP in staging

--- apps/mdn/mdn-aws/k8s/regions/oregon/stage.sh
+++ apps/mdn/mdn-aws/k8s/regions/oregon/stage.sh
@@ -109,7 +109,7 @@ export KUMA_CELERY_ALWAYS_EAGER=False
 export KUMA_CELERYD_MAX_TASKS_PER_CHILD=0
 export KUMA_CSP_ENABLE_MIDDLEWARE=True
 export KUMA_CSP_REPORT_ENABLE=True
-export KUMA_CSP_REPORT_ONLY=True
+export KUMA_CSP_REPORT_ONLY=False
 export KUMA_CSP_REPORT_URI=https://sentry.prod.mozaws.net/api/72/security/?sentry_key=25e652a045b642dfaa310e92e800058a
 export KUMA_CSRF_COOKIE_SECURE=True
 export KUMA_DEBUG=False
