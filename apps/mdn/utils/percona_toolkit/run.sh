300705b2122d1bc40274f6cf72df4b71f2cc43e6
limed <limed@mozilla.com>
2019-01-10 11:00:22 -0800

    Splitting secret and command from percona toolkit
    This splits the command and the secret up and also points the image to
    the mdnwebdocs docker hub location

--- apps/mdn/utils/percona_toolkit/run.sh
+++ apps/mdn/utils/percona_toolkit/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-${PT_CMD}
+PT_KILL_OPTIONS="--busy-time=30m --kill --print --victims=all --verbose"
+PT_KILL_CREDS="h=${DB_HOST},D=${DB_NAME},u=${DB_USER},p=${DB_PASS}"
 
+pt-kill ${PT_KILL_OPTIONS} ${PT_KILL_CREDS}
