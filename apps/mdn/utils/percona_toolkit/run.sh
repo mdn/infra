226571b9b39b52463d5acb53369e77c71864a7bd
limed <limed@mozilla.com>
2019-01-16 08:57:23 -0800

    Tune to --busy-time=15

--- apps/mdn/utils/percona_toolkit/run.sh
+++ apps/mdn/utils/percona_toolkit/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PT_KILL_OPTIONS="--busy-time=30m --kill --print --victims=all --verbose"
+PT_KILL_OPTIONS="--busy-time=15m --kill --print --victims=all --verbose"
 PT_KILL_CREDS="h=${DB_HOST},D=${DB_NAME},u=${DB_USER},p=${DB_PASS}"
 
 pt-kill ${PT_KILL_OPTIONS} ${PT_KILL_CREDS}
