9e951f67a090b6699393fa0c21fbc5d59104ed60
limed <limed@mozilla.com>
2019-06-11 08:20:05 -0700

    Allow jenkins to access the insights bucket

--- jenkins/infra/terraform/outputs.tf
+++ jenkins/infra/terraform/outputs.tf
@@ -1,8 +1,15 @@
-
 output elb_id {
   value = "${aws_elb.ci.dns_name}"
 }
 
 output backup_bucket_name {
   value = "${aws_s3_bucket.ci-backup-bucket.id}"
+}
+
+output "ci_role_name" {
+  value = "${aws_iam_role.ci.id}"
+}
+
+output "ci_role_arn" {
+  value = "${aws_iam_role.ci.arn}"
 }
