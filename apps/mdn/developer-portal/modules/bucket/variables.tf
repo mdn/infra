9357c5ee3e6c1fc093d5337830c063ccde744c8d
limed <limed@mozilla.com>
2019-07-10 16:56:45 -0700

    Create buckets required for developer-portal

--- apps/mdn/developer-portal/modules/bucket/variables.tf
+++ apps/mdn/developer-portal/modules/bucket/variables.tf
@@ -7,3 +7,7 @@ variable "environment" {}
 variable "bucket_name" {}
 
 variable "eks_worker_role_arn" {}
+
+variable "bucket_acl" {
+  default = "private"
+}
--- apps/mdn/developer-portal/outputs.tf
+++ apps/mdn/developer-portal/outputs.tf
@@ -1,3 +1,11 @@
 output "db_stage_hostname" {
   value = "${module.db_stage.hostname}"
 }
+
+output "bucket_stage_name" {
+  value = "${module.bucket_stage.bucket_id}"
+}
+
+output "bucket_stage_role_arn" {
+  value = "${module.bucket_stage.bucket_iam_role_arn}"
+}
