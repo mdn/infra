d3d6c0d7725b9395997d2aa0c694b503ef8518da
limed <limed@mozilla.com>
2019-07-30 16:18:18 -0700

    Adding storage module

--- apps/mdn/developer-portal/main.tf
+++ apps/mdn/developer-portal/main.tf
@@ -19,3 +19,10 @@ module "bucket_stage" {
   bucket_name         = "developer-portal"
   eks_worker_role_arn = "${data.terraform_remote_state.eks-us-west-2.developer_portal_worker_iam_role_arn}"
 }
+
+module "efs_stage" {
+  source               = "./modules/storage"
+  environment          = "stage"
+  subnets              = "${data.terraform_remote_state.vpc-us-west-2.private_subnets}"
+  nodes_security_group = ["${data.terraform_remote_state.eks-us-west-2.developer_portal_worker_security_group_id}"]
+}
---
+++ apps/mdn/developer-portal/modules/storage/outputs.tf
@@ -0,0 +1,4 @@
+
+output "efs_id" {
+  value = "${aws_efs_file_system.this.id}"
+}
