f6714e10d16c4dc4e1215e56f693be26bdc3ce7a
limed <limed@mozilla.com>
2019-07-30 17:28:30 -0700

    Adding missing file

--- apps/mdn/developer-portal/data.tf
+++ apps/mdn/developer-portal/data.tf
@@ -17,3 +17,13 @@ data terraform_remote_state "eks-us-west-2" {
     region = "us-west-2"
   }
 }
+
+data terraform_remote_state "kops-us-west-2" {
+  backend = "s3"
+
+  config {
+    bucket = "mdn-state-4e366a3ac64d1b4022c8b5e35efbd288"
+    key    = "terraform/kubernetes-us-west-2a"
+    region = "us-west-2"
+  }
+}
