d78e20c716d171fc93f0daa9922d7a2837231309
limed <limed@mozilla.com>
2020-03-25 12:03:16 -0700

    Update jenkins terraform to terraform 0.12

--- jenkins/infra/terraform/outputs.tf
+++ jenkins/infra/terraform/outputs.tf
@@ -1,15 +1,16 @@
-output elb_id {
-  value = "${aws_elb.ci.dns_name}"
+output "elb_id" {
+  value = aws_elb.ci.dns_name
 }
 
-output backup_bucket_name {
-  value = "${aws_s3_bucket.ci-backup-bucket.id}"
+output "backup_bucket_name" {
+  value = aws_s3_bucket.ci-backup-bucket.id
 }
 
 output "ci_role_name" {
-  value = "${aws_iam_role.ci.id}"
+  value = aws_iam_role.ci.id
 }
 
 output "ci_role_arn" {
-  value = "${aws_iam_role.ci.arn}"
+  value = aws_iam_role.ci.arn
 }
+
