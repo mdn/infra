cafc23cdd558c21c814e78438585829845166ace
limed <limed@mozilla.com>
2020-08-24 10:03:01 -0700

    STS to communicate with regional endpoint

--- k8s/tools/media-sync/media-sync.sh
+++ k8s/tools/media-sync/media-sync.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-west-2}
+AWS_STS_REGIONAL_ENDPOINTS=${AWS_STS_REGIONAL_ENDPOINTS:-regional}
 PROD_MEDIA_BUCKET=${PROD_MEDIA_BUCKET:-mdn-media-prod}
 STAGE_MEDIA_BUCKET=${STAGE_MEDIA_BUCKET:-mdn-media-stage}
 DMS_URL=${DMS_URL}
@@ -13,6 +15,9 @@ if [ -z "${DMS_URL}" ]; then
     echo "No DMS URL"
     exit 1
 fi
+
+export AWS_DEFAULT_REGION
+export AWS_STS_REGIONAL_ENDPOINTS
 
 aws s3 sync ${OPTIONS} "s3://${PROD_MEDIA_BUCKET}/" "s3://${STAGE_MEDIA_BUCKET}/"
 RV=$?
