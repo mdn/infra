3fa00110f61f4f4a1607c7764a1541c0f1fea24b
limed <limed@mozilla.com>
2020-10-21 11:30:11 -0700

    Move tunnel to us-west-2

--- apps/mdn/ssh-tunnel/main.tf
+++ apps/mdn/ssh-tunnel/main.tf
@@ -1,10 +1,10 @@
 
 module "ssh_tunnel_eu_central_1" {
   source     = "./modules/ssh-tunnel"
-  region     = "eu-central-1"
-  spot_price = "0.0019"
-  vpc_id     = data.terraform_remote_state.vpc-eu-central-1.outputs.vpc_id
-  zone_id    = data.terraform_remote_state.dns.outputs.eu-central-1-zone-id
+  region     = "us-west-2"
+  spot_price = "0.0021"
+  vpc_id     = data.terraform_remote_state.vpc-us-west-2.outputs.vpc_id
+  zone_id    = data.terraform_remote_state.dns.outputs.us-west-2-zone-id
 
   github_users = [
     "limed",
