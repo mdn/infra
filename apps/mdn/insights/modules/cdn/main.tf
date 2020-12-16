19660ef3964062283680dd625324c8c3debf1bff
limed <limed@mozilla.com>
2020-12-16 15:46:32 -0800

    Update lambda runtime

--- apps/mdn/insights/modules/cdn/main.tf
+++ apps/mdn/insights/modules/cdn/main.tf
@@ -196,7 +196,7 @@ resource "aws_lambda_function" "lambda-headers" {
   source_code_hash = data.archive_file.lambda-zip.output_base64sha256
   role             = aws_iam_role.lambda-edge-role.arn
   handler          = var.event_trigger
-  runtime          = "nodejs10.x"
+  runtime          = "nodejs12.x"
 
   tags = {
     Name        = "${local.servicename}-headers"
