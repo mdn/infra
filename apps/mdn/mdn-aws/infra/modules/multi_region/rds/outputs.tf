7e51d8906cd657fc7527dd964c8ed38fbb3be552
Brett Kochendorfer <bkochendorfer@mozilla.com>
2021-08-10 13:53:43 -0500

    SE-2280 Decom mysql
    We are moving away from Mysql to Postgres. We have backups of these
    hosts and they can now be decommed.

--- apps/mdn/mdn-aws/infra/modules/multi_region/rds/outputs.tf
+++ apps/mdn/mdn-aws/infra/modules/multi_region/rds/outputs.tf
@@ -1,19 +1,15 @@
-output "rds_arn" {
-  value = element(concat(aws_db_instance.mdn_rds.*.arn, [""]), 0)
-}
-
-output "rds_address" {
-  value = element(concat(aws_db_instance.mdn_rds.*.address, [""]), 0)
-}
-
-output "rds_endpoint" {
-  value = element(concat(aws_db_instance.mdn_rds.*.endpoint, [""]), 0)
-}
-
-output "rds_id" {
-  value = element(concat(aws_db_instance.mdn_rds.*.id, [""]), 0)
-}
-
 output "postgres_rds_arn" {
   value = element(concat(aws_db_instance.mdn_postgres.*.arn, [""]), 0)
+}
+
+output "postgres_rds_address" {
+  value = element(concat(aws_db_instance.mdn_postgres.*.address, [""]), 0)
+}
+
+output "postgres_rds_endpoint" {
+  value = element(concat(aws_db_instance.mdn_postgres.*.endpoint, [""]), 0)
+}
+
+output "postgres_rds_id" {
+  value = element(concat(aws_db_instance.mdn_postgres.*.id, [""]), 0)
 }
