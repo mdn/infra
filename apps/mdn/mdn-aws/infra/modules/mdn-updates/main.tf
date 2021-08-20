82a8eb3239006018c899cdf2dfe987e676c57ba1
Brett Kochendorfer <bkochendorfer@mozilla.com>
2021-08-20 16:32:09 -0500

    enforce https only

--- apps/mdn/mdn-aws/infra/modules/mdn-updates/main.tf
+++ apps/mdn/mdn-aws/infra/modules/mdn-updates/main.tf
@@ -164,7 +164,7 @@ resource "aws_cloudfront_distribution" "updates_distribution" {
     custom_origin_config {
       http_port                = "80"
       https_port               = "443"
-      origin_protocol_policy   = "http-only"
+      origin_protocol_policy   = "https-only"
       origin_read_timeout      = 30
       origin_ssl_protocols     = ["TLSv1.2"]
       origin_keepalive_timeout = 5
